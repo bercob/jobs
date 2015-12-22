ebbdad9a747e4682b4fafa69e6fedbf5064f85cc
Peter Balogh <bercob@gmail.com>
2015-12-22 14:11:01 +0100

    used simple_form with bootstrap

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,15 +2,17 @@ class UserSessionsController < ApplicationController
   before_action :require_login, only: [:destroy]
   def new
     @user = User.new
-    @email = params[:email]
+    if params.has_key?('user_sessions')
+      @email = params[:user_sessions][:email]
+    end
   end
 
   def create
-    if @user = login(params[:email], params[:password])
+    if @user = login(params[:user_sessions][:email], params[:user_sessions][:password])
       redirect_back_or_to(:users, notice: 'Login successful')
     else
       flash.now[:alert] = 'Login failed'
-      @email = params[:email]
+      @email = params[:user_sessions][:email]
       render action: 'new'
       #redirect_to new_user_session_path(email: params[:email]), alert: 'Login failed'
     end
