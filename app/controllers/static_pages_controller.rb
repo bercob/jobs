9e78c9cb17b181721d4526b13dfdfd7d494775f2
Peter Balogh <bercob@gmail.com>
2015-12-28 23:30:40 +0100

    trying to fix heroku deployment 01

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class StaticPagesController < ApplicationController
   def home
     doc = Nokogiri::XML(open('https://www.profesia.sk/partner/export.php?auth=241c54637eb11777b50d6ba7e9c29eba')) do |config|
