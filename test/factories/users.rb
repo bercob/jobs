363cf6c927f683b548062ef91605ad419da86206
berco <bercob@gmail.com>
2016-09-09 09:28:51 +0200

    add schema information for users

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :integer          not null, primary key
+#  email            :string           not null
+#  crypted_password :string
+#  salt             :string
+#  created_at       :datetime
+#  updated_at       :datetime
+#  admin            :boolean
+#  degree_id        :integer
+#
+# Indexes
+#
+#  index_users_on_degree_id  (degree_id)
+#  index_users_on_email      (email) UNIQUE
+#
+
 FactoryGirl.define do
   factory :user do
     factory :user_na do
